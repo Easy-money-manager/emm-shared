6b06e4f97a5e67d732a0cf7d6b9a74bf6ec55b6b
Evrandil <grzes.zapala@gmail.com>
2026-09-19 13:54:38 +0200

    just multirepo debugging

--- src/sheet.rs
+++ src/sheet.rs
@@ -8,6 +8,8 @@ pub enum SheetError {
 
 #[derive(Clone, Copy, Debug)]
 pub enum RecordSorting {
+    DescriptionAscending,
+    DescriptionDescending,
     DateAscending,
     DateDescending,
     ValueAscending,
@@ -90,6 +92,8 @@ impl Sheet {
     }
     pub fn records_sort(&mut self, record_sorting: RecordSorting) {
         match record_sorting {
+            RecordSorting::DescriptionDescending	=> self.records.sort_by_key(|record| { std::cmp::Reverse(record.description) } ),
+            RecordSorting::DescriptionAscending	=> self.records.sort_by_key(|record| record.description),
             RecordSorting::DateDescending	=> self.records.sort_by_key(|record| { std::cmp::Reverse(record.date) } ),
             RecordSorting::DateAscending	=> self.records.sort_by_key(|record| record.date),
             RecordSorting::ValueDescending	=> self.records.sort_by_key(|record| { std::cmp::Reverse(record.value) }),
